a0693037029386e84fa955945b7c208ed1df9f83
yoongyo <jyg0172@naver.com>
2020-02-06 05:36:29 +0900

    AD

--- peed/models.py
+++ peed/models.py
@@ -5,18 +5,33 @@ from ckeditor.fields import RichTextField
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
+class ChildCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Peed(models.Model):
     title = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    childCategory = models.ForeignKey(ChildCategory, on_delete=models.CASCADE)
     thumbnail = models.ImageField()
     content = RichTextField()
     created_at = models.DateField(auto_now_add=True)
-    tag = models.ManyToManyField(Tag, related_name="tag")
+    tag = models.ManyToManyField(Tag, related_name="tag", blank=True, null=True)
 
     def __str__(self):
         return self.title
