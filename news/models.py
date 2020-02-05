8ad176a132eb904217cf0e990fe5bb21223bf28c
yoongyo <jyg0172@naver.com>
2020-02-06 03:46:44 +0900

    AD

--- news/models.py
+++ news/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from ckeditor.fields import RichTextField
-import sys
-sys.path.append('..')
-from tag.models import Tag
 
 
 class NewsCategory(models.Model):
@@ -18,7 +15,6 @@ class News(models.Model):
     thumbnail = models.ImageField()
     content = RichTextField()
     created_at = models.DateField(auto_now_add=True)
-    tag = models.ManyToManyField(Tag, related_name="news_tag")
 
     def __str__(self):
         return self.title
