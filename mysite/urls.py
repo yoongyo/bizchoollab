a4ac482b44f9a16b367ec58f8a39dd569ee4f331
yoongyo <jyg0172@naver.com>
2020-02-11 11:37:40 +0900

    AD

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
-from graphene_file_upload.django import FileUploadGraphQLView
+# from graphene_file_upload.django import FileUploadGraphQLView
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^graphql/', FileUploadGraphQLView.as_view(graphiql=True)),
+    re_path(r'^graphql/', GraphQLView.as_view(graphiql=True)),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
