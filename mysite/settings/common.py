6348e81d24a91927237a4760472f1e2d2e0e5417
yoongyo <jyg0172@naver.com>
2020-02-11 12:16:41 +0900

    AD

--- mysite/settings/common.py
+++ mysite/settings/common.py
@@ -106,3 +106,23 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
