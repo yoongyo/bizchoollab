6efcc7f43b0281fe4a63cbfb0da360bf52a3c557
yoongyo <jyg0172@naver.com>
2020-02-15 07:48:00 +0900

    AD

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from graphene_file_upload.django import FileUploadGraphQLView
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^graphql/', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True))),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
