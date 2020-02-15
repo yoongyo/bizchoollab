ca018260bf77250f40afefc94f8ce9498bde6fee
yoongyo <jyg0172@naver.com>
2020-02-16 05:18:51 +0900

    AD

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -43,5 +43,5 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION_MEDIA)
-MEDIA_ROOT = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
