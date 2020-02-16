4e4e326160e653e358564117344b1174e21d7bda
yoongyo <jyg0172@naver.com>
2020-02-16 16:43:03 +0900

    AD

--- mysite/schema.py
+++ mysite/schema.py
@@ -1,6 +1,8 @@
 from feed.schema import Query as PeedQuery
 from feed.schema import Mutation as PeedMutation
 from accounts.schema import Query as UserQuery
+from accounts.schema import Mutation as UserMutation
+
 import graphene
 import graphql_jwt
 
@@ -9,7 +11,7 @@ class Query(PeedQuery, UserQuery, graphene.ObjectType):
     pass
 
 
-class Mutation(PeedMutation, graphene.ObjectType):
+class Mutation(PeedMutation, UserMutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
