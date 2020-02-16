8dd31f2fc1d13359a27d08025b1ebd0e5616ffe7
yoongyo <jyg0172@naver.com>
2020-02-16 16:37:59 +0900

    AD

--- mysite/schema.py
+++ mysite/schema.py
@@ -5,11 +5,11 @@ import graphene
 import graphql_jwt
 
 
-class Query(PeedQuery, graphene.ObjectType):
+class Query(PeedQuery, UserQuery, graphene.ObjectType):
     pass
 
 
-class Mutation(PeedMutation, UserQuery, graphene.ObjectType,):
+class Mutation(PeedMutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
